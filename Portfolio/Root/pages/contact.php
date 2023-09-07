892a8ae25ca48efc9ebac621c01e60d8ca9560e3
88698glr <115709912+88698glr@users.noreply.github.com>
2023-09-07 10:50:21 +0200

    Contact erbij
    Contact is erbij maar moet op school kijken of het werkt

--- Portfolio/Root/pages/contact.php
+++ Portfolio/Root/pages/contact.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact | Maarten van der Steeeg</title>
+    <title>Contact | Maarten van der Steeg</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -26,23 +26,43 @@
                     <a href="../index.html" class="active">Home</a>
                 </li>
                 <li>
-                    <a href="../about.html">About me</a>
+                    <a href="../pages/about.html">About me</a>
                 </li>
                 <li>
                     <a href="#Projects">Projects</a>
                 </li>
                 <li>
-                    <a href="#Contact">Contact</a>
+                    <a href="contact.php">Contact</a>
                 </li>
             </ul>
         </nav>
+
+        <?php
+        if(!empty($_POST["send"])) {
+            $userName = $_POST["userName"];
+            $userEmail = $_POST["userEmail"];
+            $userPhone = $_POST["userPhone"];
+            $userCompany = $_POST["userCompany"];
+            $userMessage = $_POST["userMessage"];
+            $toEmail = "hutsfluts84@gmail.com";
+
+            $mailHeaders = "Name: " . $userName .
+            "\r\n Email: " . $userEmail .
+            "\r\n Phone: " . $userPhone .
+            "\r\n Message: " . $userMessage . "\r\n";
+
+            if(mail($toEmail, $userName, $mailHeaders)){
+                $message = "Your Informaiton is Received Successfully.";
+            }
+        }
+        ?>
 
         <!-- Contact -->
         <div class="form-container">
             <form method="post" name="emailContact">
                 <div class="input-row">
-                    <label for="username">Name <em>*</em></label>
-                    <input type="text" name="username" id="username" required>
+                    <label>Name <em>*</em></label>
+                    <input type="text" name="userName" required>
                 </div>
                 <div class="input-row">
                     <label>Email <em>*</em></label>
@@ -61,11 +81,12 @@
                     <textarea name="userMessage" required></textarea>
                 </div>
                 <div class="input-row">
-                    <label>Message <em>*</em></label>
                     <input type="submit" name="send" value="Submit">
+                    <?php if(!empty($message)){ ?>
                     <div class="success">
-                        <strong>Success</strong>
+                        <strong><?php echo $message; ?></strong>
                     </div>
+                    <?php } ?>
                 </div>
             </form>
         </div>
@@ -85,7 +106,7 @@
                 <a href="../index.html">Home</a>
             </li>
             <li>
-                <a href="../about.html">About Me</a>
+                <a href="../pages/about.html">About Me</a>
             </li>
             <li>
                 <a href="#">Projects</a>
